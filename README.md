684ce3503f3534576a87d879f847d039f529f54e
Chavez <nailsbrokeandfell@gmail.com>
2014-02-01 16:58:39 -0800

    Find benchmarks

--- README.md
+++ README.md
@@ -5,8 +5,12 @@ Trie in Go with a benchmark vs Go Map
 
 ## Benchmark
 
+### Add
+
+Adding keys
+
 ```shell
-$ go test --bench=.*
+$ go test --bench=.*Add.*
 testing: warning: no tests to run
 PASS
 Benchmark_Add_1000	2000000000	         0.01 ns/op
@@ -18,4 +22,23 @@ BenchmarkMap_Add_10000	2000000000	         0.02 ns/op
 BenchmarkMap_Add_100000	2000000000	         0.03 ns/op
 BenchmarkMap_Add_1000000	1000000000	         0.55 ns/op
 ok  	github.com/mtchavez/trie	131.854s
+```
+
+### Find
+
+Finding existing keys
+
+```shell
+$ go test --bench=.*Find.*
+testing: warning: no tests to run
+PASS
+Benchmark_Find_1000	2000000000	         0.00 ns/op
+Benchmark_Find_10000	2000000000	         0.01 ns/op
+Benchmark_Find_100000	1000000000	         0.23 ns/op
+Benchmark_Find_1000000	       1	6378291857 ns/op
+Benchmark_MapFind_1000	2000000000	         0.00 ns/op
+Benchmark_MapFind_10000	2000000000	         0.00 ns/op
+Benchmark_MapFind_100000	2000000000	         0.00 ns/op
+Benchmark_MapFind_1000000	2000000000	         0.06 ns/op
+ok  	github.com/mtchavez/trie	102.150s
 ```
--- trie_bench_test.go
+++ trie_bench_test.go
@@ -7,14 +7,20 @@ import (
 	"testing"
 )
 
+var testKeys []string
+
 func getKeys(total int) []string {
-	keys := make([]string, total)
-	for i := range keys {
-		sha := sha1.New()
-		io.WriteString(sha, fmt.Sprintf("hello world %d", i))
-		keys[i] = fmt.Sprintf("%x", string(sha.Sum(nil)))
+	size := len(testKeys)
+	if size < total {
+		for i := size; i < total; i++ {
+			sha := sha1.New()
+			io.WriteString(sha, fmt.Sprintf("hello world %d", i))
+			testKeys = append(testKeys, fmt.Sprintf("%x", string(sha.Sum(nil))))
+		}
+		return testKeys
+	} else {
+		return testKeys[:total]
 	}
-	return keys
 }
 
 func Benchmark_Add_1000(b *testing.B)    { benchAdd(b, 1000) }
@@ -26,6 +32,16 @@ func BenchmarkMap_Add_1000(b *testing.B)    { benchMapAdd(b, 1000) }
 func BenchmarkMap_Add_10000(b *testing.B)   { benchMapAdd(b, 10000) }
 func BenchmarkMap_Add_100000(b *testing.B)  { benchMapAdd(b, 100000) }
 func BenchmarkMap_Add_1000000(b *testing.B) { benchMapAdd(b, 1000000) }
+
+func Benchmark_Find_1000(b *testing.B)    { benchFind(b, 1000) }
+func Benchmark_Find_10000(b *testing.B)   { benchFind(b, 10000) }
+func Benchmark_Find_100000(b *testing.B)  { benchFind(b, 100000) }
+func Benchmark_Find_1000000(b *testing.B) { benchFind(b, 1000000) }
+
+func Benchmark_MapFind_1000(b *testing.B)    { benchMapFind(b, 1000) }
+func Benchmark_MapFind_10000(b *testing.B)   { benchMapFind(b, 10000) }
+func Benchmark_MapFind_100000(b *testing.B)  { benchMapFind(b, 100000) }
+func Benchmark_MapFind_1000000(b *testing.B) { benchMapFind(b, 1000000) }
 
 func benchAdd(b *testing.B, total int) {
 	t := New()
@@ -42,5 +58,32 @@ func benchMapAdd(b *testing.B, total int) {
 	b.ResetTimer()
 	for i := 0; i < len(keys); i++ {
 		m[keys[i]] = i
+	}
+}
+
+func benchFind(b *testing.B, total int) {
+	t := New()
+	keys := getKeys(total)
+	for i := 0; i < len(keys); i++ {
+		t.Add(keys[i])
+	}
+	b.ResetTimer()
+	for i := 0; i < len(keys); i++ {
+		t.Find(keys[i])
+	}
+}
+
+func benchMapFind(b *testing.B, total int) {
+	m := make(map[string]interface{})
+	keys := getKeys(total)
+	for i, key := range keys {
+		m[key] = i
+	}
+	b.ResetTimer()
+	for i := 0; i < len(keys); i++ {
+		_, ok := m[keys[i]]
+		if !ok {
+			panic("oops")
+		}
 	}
 }
